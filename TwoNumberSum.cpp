13b207f2e3cb0aff436317b88d47feb4cd60433c
Md. Anwar Hossain <anwar9437@gmail.com>
2022-03-06 16:50:34 +0600

    two number sum

---
+++ TwoNumberSum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int number1= 10;
+    float number2 = 10.20;
+    char ch = 'a';
+
+    cout<<"Number 1= "<<number1<< endl <<"Number 2 =" <<number2 << endl;
+
+    float sum= number1+number2;
+
+    cout << "Total Sum =" << sum;
+}
