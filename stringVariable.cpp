a068a0a9b1e649e6b3a5caea18e2a6e6aba5e2df
Md. Anwar Hossain <anwar9437@gmail.com>
2022-03-07 12:28:34 +0600

    string variable declaration and initialization

---
+++ stringVariable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    char name[20]= "Md. Anwar Hossain";
+    char address [100]= "459/3 south kafrul, dhaka-1206";
+
+    cout<<"My name is : "<< name << endl;
+    cout<<"address: "<< address << endl;
+    getch();
+}
