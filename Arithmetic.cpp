69a111287bfa3a61c8318f882ebe555a28fdc9f2
Md. Anwar Hossain <anwar9437@gmail.com>
2022-03-11 12:51:50 +0600

    use show point & fixed set precision

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -1,29 +1,31 @@
 #include<iostream>
 #include<conio.h>
+#include<iomanip>
 
 using namespace std;
 
 int main(){
-    int num1, num2,sum,sub,multi ;
+    float num1, num2,sum,sub,multi ;
     double div, rem ;
 
     cout<< "Enter two Number :" <<endl;
     cin>> num1 >> num2;
 
+    cout <<fixed<<setprecision(5);
     sum= num1 + num2;
-    cout<< "sum is :"<< sum<< endl;
+    cout <<setw(20 )<< "sum is :"<< sum<< endl;
+
 
     sub= num1 - num2 ;
-    cout<< "subtraction is :"<< sub<< endl;
+    cout<<setw(20 )<< "subtraction is :"<< sub<< endl;
+
 
     multi = num1 * num2;
-    cout << "multiple is : " <<multi<< endl;
+    cout<<setw(20 ) << "multiple is : " <<multi<< endl;
 
+    cout<<fixed<<setprecision(7);
     div = (float )num1 / num2;
-    cout << "division is : "<< div <<endl;
-
-    rem = num1 % num2 ;
-    cout <<"Remainder is : "<< rem << endl;
+    cout<<setw(20) << "division is : "<< div <<endl;
 
 
     getch();
--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!"<< std::endl <<"\"Hello Everybody\"" <<std::endl;
     std::cout << "Hello, Bangladesh !!!" << std::endl;
+    std::cout<< "Hi , programmer "  << std::endl;
     return 0;
 }
