53df4bf5f13c2c3349257f7b6920438801d15eb2
Md. Anwar Hossain <anwar9437@gmail.com>
2022-03-12 12:34:41 +0600

    calculate Rectangle area

---
+++ rectangle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+
+using  namespace std;
+
+int main(){
+    double width, height, area;
+
+    cout << " Enter width :"<< endl;
+    cin >> width;
+
+    cout << " Enter height :"<< endl;
+    cin>> height;
+
+    area = width * height;
+
+    cout << fixed << setprecision(4);
+    cout << "Area of rectangle :"<< area << endl;
+
+    getch();
+
+
+
+}
+
+
+
