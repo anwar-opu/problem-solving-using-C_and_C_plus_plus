e7a3305ca1e1dc2d5d2a3f3101c16e8b51f69d1c
Md. Anwar Hossain <anwar9437@gmail.com>
2022-04-02 12:12:59 +0600

    hacker rank Array introduction  problem solved

--- Arrays_Introduction.cpp
+++ Arrays_Introduction.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+#define MAX 1000
 using namespace std;
 
 int main(){
-    int a[10],n;
 
-    cout<<"enter array size:";
-    cin>> n;
-    for (int i = 0; i < n; i++) {
-        cout<<a[i]<<endl;
+    int a[MAX],n;
+
+    cin>> n;                         //array size
+
+    for (int i = 1; i <=n; i++) {   //array input
+        cin>>a[i];
+    }
+
+    for (int i = 0; i < n; i++) {   //array output
+        cout<<a[n-i]<<" ";
     }
 
 }
