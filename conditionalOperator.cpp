a253a241ea23d35aae28c02ea03236d7934f3689
Md. Anwar Hossain <anwar9437@gmail.com>
2022-05-26 15:01:43 +0600

    Conditional or ternary Operator

---
+++ conditionalOperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<< " Enter any integer number : " << endl;
+    cin>> num;
+
+    (num % 2 == 0) ? cout<<num<< " is number even "   :  cout<<num<< " is number odd ";
+
+
+
+}
+
