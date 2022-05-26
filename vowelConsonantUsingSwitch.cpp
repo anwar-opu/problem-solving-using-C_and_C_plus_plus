b12935f88650f4dbd4deb88f5cffa8740dbc3cb1
Md. Anwar Hossain <anwar9437@gmail.com>
2022-05-26 15:34:19 +0600

    vowel & consonant  using switch  & tolower function

--- vowelConsonantUsingSwitch.cpp
+++ vowelConsonantUsingSwitch.cpp
@@ -7,39 +7,18 @@ int main (){
 
     cout<<"Enter any letter :"<< endl;
     cin>>ch;
+    ch = tolower(ch);
 
     switch (ch) {
 
         case 'a':
-            cout<<"vowel"<<endl;
-            break;
         case 'e':
-            cout<<"vowel"<<endl;
-            break;
         case 'i':
-            cout<<"vowel"<<endl;
-            break;
         case 'o':
-            cout<<"vowel"<<endl;
-            break;
         case 'u':
             cout<<"vowel"<<endl;
             break;
-        case 'A':
-            cout<<"vowel"<<endl;
-            break;
-        case 'E':
-            cout<<"vowel"<<endl;
-            break;
-        case 'I':
-            cout<<"vowel"<<endl;
-            break;
-        case 'O':
-            cout<<" vowel "<<endl;
-            break;
-        case 'U':
-            cout<<"vowel"<<endl;
-            break;
+
         default:
             cout<<"consonant"<<endl;
     }
