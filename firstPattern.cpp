267c816e795abcfc1c64b1a89c1ba6cfaed3d7ea
Md. Anwar Hossain <anwar9437@gmail.com>
2022-05-31 17:05:27 +0600

    first patten problem

---
+++ firstPattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int  n, row, col;
+
+    cout<<" Enter  number of line :"<<endl;
+    cin>>n;
+
+    for (row = 1; row <= n; row++) {
+        for ( col = 1; col <= row ; col++) {
+            cout <<" "<< col ;
+        }
+        cout<<endl;
+    }
+
+}
