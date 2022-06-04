37a4a9f5cf1aad87af65fada6ce37d9ce097b9a7
Md. Anwar Hossain <anwar9437@gmail.com>
2022-06-05 01:02:08 +0600

    random number using function

---
+++ RandomNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+
+
+    for (int i = 0; i < 5; i++) {
+        int randomNummber= rand();
+        cout<< "random number = :"<< randomNummber << endl;
+    }
+
+}
+
