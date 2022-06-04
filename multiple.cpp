8856cd5d874fd2f8b6335313184b11df44e857ed
Md. Anwar Hossain <anwar9437@gmail.com>
2022-06-04 23:55:28 +0600

    addition,subtract,multiplication, division using function

--- multiple.cpp
+++ multiple.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+
 using namespace std;
 
 
@@ -6,14 +7,23 @@ int addition (int a , int b){
     int sum = a + b;
     return sum;
 }
+
 int subtract (int a , int b){
     int sub = a-b;
     return sub;
 
 }
+
 int multiplication (int a , int b){
     int mul = a * b;
     return mul;
+}
+
+void division (int a, int b)
+{
+    float div = (float)a / b;
+
+    cout<< "division  = " << div<< endl;
 }
 
 int main (){
@@ -26,8 +36,10 @@ int main (){
     sum = addition(a,b);
     sub = subtract(a,b);
     mul = multiplication(a,b);
+    division(a,b);
 
     cout<< "addition  = " << sum<< endl;
     cout<< "subtract  = " << sub<< endl;
     cout<< "multiplication  = " << mul << endl;
+
 }
