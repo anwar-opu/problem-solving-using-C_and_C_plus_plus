66698dc0d1d04130cab656dba8b5d2546071e2dd
anwar-opu <anwar9437@gmail.com>
2022-08-06 00:18:26 +0600

    find size of array

---
+++ countArraysize.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define SIZE(a) (sizeof(a)/sizeof (a[0])) // int size 4
+int main(){
+    int a[]={20,34,  10,23,56,67,89,45,34,55,90};
+
+    int b[]={56,34,78,32,56,78,3,4,5,6};
+
+
+    printf("Size of array a %d \n", SIZE(a) );
+    printf("Size of array b %d \n", sizeof(b)/sizeof(a[0]));
+
+}
