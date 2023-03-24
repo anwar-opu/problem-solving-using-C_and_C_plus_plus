a991e940afd416ca0d80746f57a416ab36559e08
anwar-opu <anwar9437@gmail.com>
2023-03-25 00:41:22 +0600

    1335A - Candies and Two Sisters codeForce solution

---
+++ 1335A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        long long int numberOfcandies,div;
+        cin>>numberOfcandies;
+
+        div = (numberOfcandies-1)/2;
+        cout<<div<<endl;
+    }
+
+    return 0;
+
+}
