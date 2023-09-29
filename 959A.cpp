fb89aa1db0d9b5820bb260443431eadb31c86cac
anwar-opu <anwar9437@gmail.com>
2023-09-29 23:18:04 +0600

    A. Mahmoud and Ehab and the even-odd game codeForce solution

---
+++ 959A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(0)
+using namespace std;
+int main(){
+    FastIO;
+    ll n;
+    cin>>n;
+    if(n%2 == 0){
+        cout<<"Mahmoud"<<endl;
+    }else
+        cout<<"Ehab"<<endl;
+    return 0;
+
+}
