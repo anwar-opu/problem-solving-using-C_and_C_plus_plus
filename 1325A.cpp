428cab9f63a8df50ef263f2bb6e546968dbbd55b
anwar-opu <anwar9437@gmail.com>
2023-11-04 12:14:59 +0600

    A. EhAb AnD gCd codeForce solution

---
+++ 1325A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(0)
+using namespace std;
+int main(){
+    FastIO;
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<"1 "<<x-1<<"\n";
+    }
+    return 0;
+}
