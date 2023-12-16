9fbc66513a5f36430aa8fbd53b4d9e76c8d62d3e
anwar-opu <anwar9437@gmail.com>
2023-12-16 18:57:50 +0600

    A. Ichihime and Triangle codeForce solution

---
+++ 1337A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(0)
+using namespace std;
+int main(){
+    FastIO;
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << endl;
+    }
+
+}
