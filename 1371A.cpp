3d5212cfc8c759869ea3ab62dd4f258622ff7c41
anwar-opu <anwar9437@gmail.com>
2024-01-02 17:23:26 +0600

    A. Magical Sticks codeForce solution

---
+++ 1371A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(0)
+using namespace std;
+int main() {
+    FastIO;
+    ll t;
+    cin>>t;
+    while(t--) {
+        vector<int> v;
+        ll n;
+        cin >> n;
+        cout << (((n - 1) / 2) + 1) << endl;
+
+    }
+
+}
